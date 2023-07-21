2af6ed73254042508ca537611946991e6c7e4760
krishna kumar <kri821084@gmail.com>
2023-07-21 11:34:40 +0530

    render by loop

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@
 import ExpenseItem from "./components/ExpenseItem"
 
 function App() {
-  return <div>
+
+let data=[]
+for (let index = 0; index < 10; index++) {
+  data[index]={
+  title:`product${index} expense`,
+  amount:Math.floor(Math.random()*10)*10,
+  }
   
+}
+console.log(data,"7")
+  return <div>
+
   <div>this is krishna</div>
-  <ExpenseItem/>
- 
+  {/* <ExpenseItem /> */}
+ {
+  data.map(()=>{
+    <ExpenseItem></ExpenseItem>
+  })
+ }
   </div>
  
 }
