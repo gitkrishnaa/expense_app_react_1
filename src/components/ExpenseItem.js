34949062da45ba1c4f6b705d23b725e5458dc9ed
krishna kumar <kri821084@gmail.com>
2023-07-21 11:25:04 +0530

    fist commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+
+function expense(){
+
+        return <div>
+            
+        <div>date:4/5/2023</div>
+        <div>name:krishn</div>
+        <div>amount:500</div>
+            
+            </div>
+
+}
+export default expense
