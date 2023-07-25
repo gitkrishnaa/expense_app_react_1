f2f98f299c343b12b4f1777f2e0b289182fe7b3e
krishna kumar <kri821084@gmail.com>
2023-07-25 17:51:38 +0530

    onclick event and use state learned and delete button added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 
 import Card from "./ui/card"
-import Left_side_menu from "./expense/main"
+import Expense_main from "./expense/main"
 // import Display from "./display/main"
 import "./App.css"
 function App() {
 
 
   return <div className="main_div1">
+<div className="main_div2">
+<Expense_main title="milk" amount="654"/>
+<Expense_main title="sdfcscsd" amount="354"/>
+<Expense_main title="sdvds" amount="654"/>
+<Expense_main title="dscvsd" amount="750"/>
 
-
-
-
-
-
- 
+</div>
 
 
   </div>
--- src/expense/main.js
+++ src/expense/main.js
@@ -1,9 +1,21 @@
-import React from 'react'
-
-function main() {
+import React,{useState} from 'react'
+import "./main.css"
+function Expense(props) {
+const [title,setTitle]=useState(props.title)
+ const amount=props.amount;
+// const title="hgh"
+ const eventHandler=()=>{
+    // alert("hi")
+    setTitle("deleted itemx")
+}
   return (
-    <div>hello dearsdfsd</div>
+    <div className='div1'>
+<div className='sub_div1'>{title}</div>
+<div className='sub_div2'>{amount}</div>
+<button onClick={eventHandler}>delete</button>
+
+    </div>
   )
 }
 
-export default main
+export default Expense
