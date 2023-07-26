9e2476830e3a10d1bf7c709c358c6be536494d64
krishna kumar <kri821084@gmail.com>
2023-07-26 14:08:49 +0530

    data gathering from input filed like tile,amount,date etc

--- src/expense/new_expense/expense_form.js
+++ src/expense/new_expense/expense_form.js
@@ -1,12 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./main.css"
 
 function Expense_form() {
+const [title,setTitle]=useState("")
+const [amount,setAmount]=useState("")
+const [date,setDate]=useState("")
+const final_data={
+    title,amount,date
+}
 
+const title_handler=(e)=>{
+// alert(e.target.value)
+setTitle(e.target.value)
+// setTitle((x)=>{
+// console.log("setTitle(e)=e-",x)
+// return {...final_data,title:e.target.value,}
+// })
+
+}
+const amount_handler=(e)=>{
+    // alert(e.target.value)
+    setAmount(e.target.value)
+}
+const date_handler=(e)=>{
+    // alert(e.target.value)
+    setDate(e.target.value)
+}
 const submit=(e)=>{
    e.preventDefault();
 //    alert("h")
-console.log(e.target.value,"form")
+// console.log(e.target.value,"form")
+console.log("form data")
+
+console.log(final_data)
 }
 
 
@@ -16,12 +42,13 @@ console.log(e.target.value,"form")
 <form  onSubmit={submit}>
     
 <div className='expense_form_sub_div1'>
-    <input type="text" placeholder='Title' />
-    <input type="number" placeholder='Amount' />
+    <input type="text" placeholder='Title' onChange={title_handler} />
+    <input type="number" onChange={amount_handler} placeholder='Amount'  />
 
 </div>
 
-<div className='expense_form_sub_div2 expense_form_sub_div1' ><input type="date" placeholder='Date'  />
+<div className='expense_form_sub_div2 expense_form_sub_div1' >
+    <input type="date" onChange={date_handler} placeholder='Date'  />
 <button type='submit' className='expense_form_btn1'>Submit</button>
 </div>
 
